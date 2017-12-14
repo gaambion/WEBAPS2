56769c8ae6c642843eb17e9e9ffe057bb1b2a2d1
mavicreccion <mavicreccion@users.noreply.github.com>
2017-12-14 11:23:55 +0800

    Retrieved values from form of Journal Add Modal

--- client/js/journal_components.js
+++ client/js/journal_components.js
@@ -3,17 +3,15 @@ class JournalBox extends React.Component {
     return(
       <div>
         {/*Filter bar*/}
-        <div class="container w-75 mx-auto">
-          <JournalFilterForm />
-        </div>
+        <JournalFilterForm />
 
         {/*Journal List*/}
         <JournalList />
 
         {/*Floating Button*/}
-        <div class="btn-group">
-            <button type="button" id="addEntry" class="btn btn-warning btn-fab pmd-btn-fab pmd-btn-raised " data-toggle="modal" data-target="#myModal">
-              <i class="fa fa-plus" aria-hidden="true"></i>
+        <div className="btn-group">
+            <button type="button" id="addEntry" className="btn btn-warning btn-fab pmd-btn-fab pmd-btn-raised " data-toggle="modal" data-target="#myModal">
+              <i className="fa fa-plus" aria-hidden="true"></i>
             </button>
         </div>
 
@@ -31,144 +29,152 @@ class JournalBox extends React.Component {
 class JournalFilterForm extends React.Component {
   render() {
     return(
-      <form>
-          <div class="form-row">
-              <label class="col-md-2 col-sm-2" for="select_month">Filter By:</label>
-              <select class="form-control col-md-2 col-sm-2" id="select_month">
-                  <option>January</option>
-                  <option>February</option>
-                  <option>March</option>
-                  <option>April</option>
-                  <option>May</option>
-                  <option>June</option>
-                  <option>July</option>
-                  <option>August</option>
-                  <option>September</option>
-                  <option>October</option>
-                  <option>November</option>
-                  <option>December</option>
-              </select>
-              <select class="form-control col-md-2 col-sm-2 ml-2" id="select_year">
-                  <option>2017</option>
-              </select>
-          </div>
-      </form>
+      <div className="container w-75 mx-auto">
+        <form>
+            <div className="form-row">
+                <label className="col-md-2 col-sm-2" htmlFor="select_month">Filter By:</label>
+                <select className="form-control col-md-2 col-sm-2" id="select_month">
+                    <option>January</option>
+                    <option>February</option>
+                    <option>March</option>
+                    <option>April</option>
+                    <option>May</option>
+                    <option>June</option>
+                    <option>July</option>
+                    <option>August</option>
+                    <option>September</option>
+                    <option>October</option>
+                    <option>November</option>
+                    <option>December</option>
+                </select>
+                <select className="form-control col-md-2 col-sm-2 ml-2" id="select_year">
+                    <option>2017</option>
+                </select>
+            </div>
+        </form>
+      </div>
     );
   }
 }
 
 class JournalAddEntryModal extends React.Component {
+
+  constructor() {
+    super();
+  }
+
   render() {
     return(
-      <div class="modal " id="addEntryModal">
-          <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                  <div class="modal-header">
-                      <h5 class="modal-title">November 17, 2017</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal " id="addEntryModal">
+          <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                  <div className="modal-header">
+                      <h5 className="modal-title">November 17, 2017</h5>
+                      <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                       </button>
                   </div>
-                  <div class="modal-body">
-                      <JournalAddForm/>
+                  <div className="modal-body">
+                    <form id="new-entry" className="" action="index.html" method="post">
+                        <div className="form-group">
+                            <label htmlFor="entryTitle">Title</label>
+                            <input type="text" className="form-control" id="entryTitle" placeholder="My Entry" name="title"
+                              ref={(input) => this._title = input}/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="newEntry">Journal Entry</label>
+                            <textarea className="form-control" rows="5" id="newEntry" name="body"
+                              ref={(textarea) => this._entry = textarea}></textarea>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="select_category">Category</label>
+                            <select className="form-control " id="select_category" name="category"
+                              ref={(select) => this._category = select}>
+                                <option>Thoughts</option>
+                                <option>Travel</option>
+                                <option>School</option>
+                                <option>Love</option>
+                                <option>Friends</option>
+                                <option>Notes</option>
+                                <option>Others</option>
+                            </select>
+                        </div>
+                    </form>
                   </div>
-                  <div class="modal-footer">
-                      <button type="submit" class="btn btn-primary closeModal">Save</button>
-                      <button type="button" class="btn btn-secondary closeModal" data-dismiss="modal">Close</button>
+                  <div className="modal-footer">
+                      <button type="submit" className="btn btn-primary closeModal"
+                        onClick={this._handleClick.bind(this)}>Save</button>
+                      <button type="button" className="btn btn-secondary closeModal" data-dismiss="modal">Close</button>
                   </div>
               </div>
           </div>
       </div>
     );
   }
-}
 
-class JournalAddForm extends React.Component {
+  _handleClick(e) {
+    e.preventDefault();
 
-  render() {
-    return(
-      <form id="new-entry" className="" action="index.html" method="post">
-          <div className="form-group">
-              <label htmlFor="entryTitle">Title</label>
-              <input type="text" class="form-control" id="entryTitle" placeholder="My Entry" name="title"/>
-          </div>
+    let meeting = {
+      title: this._title.value,
+      entry: this._entry.value,
+      category: this._category.value
+    }
 
-          <div className="form-group">
-              <label htmlFor="newEntry">Journal Entry</label>
-              <textarea className="form-control" rows="5" id="newEntry" name="body"></textarea>
-          </div>
-
-          <div className="form-group">
-              <label htmlFor="select_category">Category</label>
-              <select className="form-control " id="select_category" name="category">
-                  <option>Thoughts</option>
-                  <option>Travel</option>
-                  <option>School</option>
-                  <option>Love</option>
-                  <option>Friends</option>
-                  <option>Notes</option>
-                  <option>Others</option>
-              </select>
-          </div>
-      </form>
-    );
+    console.log("title: " + this._title.value);
+    console.log("entry: " + this._entry.value);
+    console.log("category: " + this._category.value);
   }
 }
 
 class JournalOpenEntryModal extends React.Component {
   render() {
     return(
-      <div class="modal modal-ku" id="viewEntry">
-          <div class="modal-dialog" role="document">
-              <div class="modal-content ">
-                  <div class="modal-header">
-                      <h5 class="modal-title">This is Untitled.</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal modal-ku" id="viewEntry">
+          <div className="modal-dialog" role="document">
+              <div className="modal-content ">
+                  <div className="modal-header">
+                      <h5 className="modal-title">This is Untitled.</h5>
+                      <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                       </button>
                   </div>
-                  <div class="modal-body">
-                      <JournalOpenForm/>
+                  <div className="modal-body">
+                    <form>
+                        <div className="row form-group">
+                            <div className="col-sm-2 text-sm-left">
+                                <label htmlFor="select_category">Category</label>
+                            </div>
+                            <div className="col-md-5">
+                                <select className="form-control col-sm-7" id="select_category">
+                                    <option>Thoughts</option>
+                                    <option>Travel</option>
+                                    <option>School</option>
+                                    <option>Love</option>
+                                    <option>Friends</option>
+                                    <option>Notes</option>
+                                    <option>Others</option>
+                                </select>
+                            </div>
+                            <div className="col-sm-4 text-sm-right">November 17, 2017</div>
+                          </div>
+                        <div className="form-group">
+                            <label htmlFor="editEntry">Body</label>
+                            <textarea className="form-control edit-body" rows="10" id="editEntry">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam tempor vestibulum. Curabitur vel purus ac nisi rutrum bibendum. Mauris nisl sapien, ornare eu maximus quis , porta sed est. Fusce ut tortor ac dolor tempor interdum quis sit amet odio. Integer rhoncus eleifend lorem non tempor. Integer tristique, ante non gravida bibendum, magna turpis sollicitudin arcu, ut viverra felis magna et erat. Nunc ultrices augue in venenatis elementum.
+                            </textarea>
+                        </div>
+                    </form>
                   </div>
-                  <div class="modal-footer">
-                      <button type="button" class="btn btn-primary closeModal">Save</button>
-                      <button type="button" class="btn btn-secondary closeModal" data-dismiss="modal">Close</button>
+                  <div className="modal-footer">
+                      <button type="button" className="btn btn-primary closeModal">Save</button>
+                      <button type="button" className="btn btn-secondary closeModal" data-dismiss="modal">Close</button>
                   </div>
               </div>
           </div>
       </div>
-    );
-  }
-}
-
-class JournalOpenForm extends React.Component {
-  render() {
-    return(
-      <form>
-          <div class="row form-group">
-              <div class="col-sm-2 text-sm-left">
-                  <label for="select_category">Category</label>
-              </div>
-              <div class="col-md-5">
-                  <select class="form-control col-sm-7" id="select_category">
-                      <option>Thoughts</option>
-                      <option>Travel</option>
-                      <option>School</option>
-                      <option>Love</option>
-                      <option>Friends</option>
-                      <option>Notes</option>
-                      <option>Others</option>
-                  </select>
-              </div>
-              <div class="col-sm-4 text-sm-right">November 17, 2017</div>
-            </div>
-          <div class="form-group">
-              <label for="editEntry">Body</label>
-              <textarea class="form-control edit-body" rows="10" id="editEntry">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam tempor vestibulum. Curabitur vel purus ac nisi rutrum bibendum. Mauris nisl sapien, ornare eu maximus quis , porta sed est. Fusce ut tortor ac dolor tempor interdum quis sit amet odio. Integer rhoncus eleifend lorem non tempor. Integer tristique, ante non gravida bibendum, magna turpis sollicitudin arcu, ut viverra felis magna et erat. Nunc ultrices augue in venenatis elementum.
-              </textarea>
-          </div>
-      </form>
     );
   }
 }
