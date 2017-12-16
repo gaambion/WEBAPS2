4b86cd47a907acff8be07adf5431b67d0922c2a9
gaambion <gaambion@gmail.com>
2017-12-16 09:40:58 +0800

    Added the model for users

--- client/server/router.js
+++ client/server/router.js
@@ -1,7 +1,9 @@
 module.exports = function(app) {
 
     var journal = require('./controller/journal.server.controller.js');
+    var user = require('./controller/user.server.controller.js');
 
+    //For journal controls
     // Create a new Note
     app.post('/journal', journal.create);
 
@@ -16,4 +18,20 @@ module.exports = function(app) {
 
     // Delete a Note with noteId
     app.delete('/journal/:noteId', journal.delete);
+
+    //For user controls
+    //Create new user
+    app.post('/user', user.create);
+
+    // Retrieve all users
+    app.get('/user', user.findAll);
+
+    // Retrieve a single user with noteId
+    app.get('/user/:userId', user.findOne);
+
+    // Update a user with noteId
+    app.put('/user/:userId', user.update);
+
+    // Delete a user with noteId
+    app.delete('/user/:userId', user.delete);
 }
